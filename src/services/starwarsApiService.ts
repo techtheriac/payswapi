b3c3b30e797b65167a44a7285374c49e2df6e7e8
Jezreel Franklin <mavyjayann@gmail.com>
2024-10-22 13:24:58 +0100

    Implement getPeopleById

--- src/services/starwarsApiService.ts
+++ src/services/starwarsApiService.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { ApiResponse } from "../types";
-import { filterObject } from "../utils/filter";
 const SWAPI_BASE = process.env.SWAPI_BASE || "https://swapi.dev/api";
 
 export interface StarwarsPerson {
@@ -12,9 +11,9 @@ export interface StarwarsPerson {
   eye_color: string;
   birth_year: string;
   gender: string;
-  homeworld: string;
-  films: string[];
-  vehicles: string[];
+  homeworld?: string;
+  films?: string[];
+  vehicles?: string[];
 }
 
 type StarwarsPeople = Omit<StarwarsPerson, "homeworld" | "films" | "vehicles">;
@@ -23,8 +22,16 @@ interface StarwarsPeopleResponse {
   results: StarwarsPeople[];
 }
 
-interface HomeWorldResponse {
+interface MetaDataResponse {
   name: string;
+}
+
+interface HomeWorldResponse extends MetaDataResponse {}
+
+interface VehilclesResponse extends MetaDataResponse {}
+
+interface FilmsResponse {
+  title: string;
 }
 
 export async function getPeople(): Promise<ApiResponse<StarwarsPeople[]>> {
@@ -72,17 +79,90 @@ export async function getPeopleById(
 ): Promise<ApiResponse<StarwarsPerson>> {
   try {
     const res = await axios.get<StarwarsPerson>(`${SWAPI_BASE}/people/${id}`);
-    const homeworldResponse = await axios.get<HomeWorldResponse>(
-      res.data.homeworld!
-    );
 
-    const response: StarwarsPerson = ({
-      homeworld: homeworldResponse.data.name,
-    } = res.data);
+    if (!res.data) {
+      return {
+        success: false,
+      };
+    }
+
+    const homeworldResponse = getMetaData<HomeWorldResponse>([
+      res.data.homeworld!,
+    ]);
+    const vehiclesResponse = getMetaData<VehilclesResponse>(res.data.vehicles);
+    const filmsResponse = getMetaData<FilmsResponse>(res.data.films);
+
+    const responses = await Promise.all([
+      homeworldResponse,
+      vehiclesResponse,
+      filmsResponse,
+    ]);
+
+    if (
+      responses.some((x) => !x.success) ||
+      responses.some((x) => !x.response)
+    ) {
+      return {
+        success: false,
+        error: { message: "unable to spool person meta data" },
+      };
+    }
+
+    const [homeworld, vehicles, films] = responses;
+
+    const response: StarwarsPerson = {
+      birth_year: res.data.birth_year,
+      eye_color: res.data.eye_color,
+      films: films.response?.map((x) => x.title),
+      gender: res.data.gender,
+      hair_color: res.data.hair_color,
+      height: res.data.height,
+      homeworld: homeworld.response?.flatMap((x) => x.name).join(),
+      vehicles: vehicles.response?.map((x) => x.name),
+      mass: res.data.mass,
+      name: res.data.name,
+      skin_color: res.data.skin_color,
+    };
 
     return {
       response,
       success: true,
+    };
+  } catch (error) {
+    console.error(error);
+
+    if (error instanceof Error) {
+      return {
+        success: false,
+        error: { message: error.message },
+      };
+    }
+
+    return {
+      success: false,
+      error: {
+        message: "an unexpected error occured whilst retrieving people",
+      },
+    };
+  }
+}
+
+async function getMetaData<T>(paths?: string[]): Promise<ApiResponse<T[]>> {
+  try {
+    if (!paths) {
+      return {
+        success: false,
+        error: { message: "you have supplied an invalid path" },
+      };
+    }
+
+    const responses = await Promise.all(
+      paths.map((path) => axios.get<T>(path))
+    );
+
+    return {
+      success: true,
+      response: responses?.map((res) => res?.data),
     };
   } catch (error) {
     console.error(error);
