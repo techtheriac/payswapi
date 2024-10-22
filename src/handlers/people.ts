d7459aefd74da0f0ada3790e91327e343395ae5d
Jezreel Franklin <mavyjayann@gmail.com>
2024-10-22 14:55:43 +0100

    Add cache to people service

--- src/handlers/people.ts
+++ src/handlers/people.ts
@@ -1,7 +1,21 @@
 import { Request, Response } from "express";
 import { getPeople, getPeopleById } from "../services/peopleApiService";
 
+import Cache from "../utils/cache";
+
+const cache = new Cache();
+
+const PERSONS_CACHE_KEY = "persons";
+const PERSON_CACHE_KEY = (id: string) => `person-${id}`;
+
 export const getAllPersons = async (req: Request, res: Response) => {
+  const personsCache = cache.get(PERSONS_CACHE_KEY);
+
+  if (personsCache) {
+    res.json(personsCache);
+    return;
+  }
+
   const response = await getPeople();
 
   if (!response.success) {
@@ -10,11 +24,20 @@ export const getAllPersons = async (req: Request, res: Response) => {
     return;
   }
 
+  cache.set(PERSONS_CACHE_KEY, response);
+
   res.json(response);
 };
 
 export const getOnePerson = async (req: Request, res: Response) => {
   const id = req.params.id;
+
+  const personCache = cache.get(PERSON_CACHE_KEY(id));
+
+  if (personCache) {
+    res.json(personCache);
+    return;
+  }
 
   const response = await getPeopleById(id);
 
@@ -23,6 +46,8 @@ export const getOnePerson = async (req: Request, res: Response) => {
     res.json(response);
     return;
   }
+
+  cache.set(PERSON_CACHE_KEY(id), response);
 
   res.json(response);
 };
