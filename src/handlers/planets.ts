9e4d0276d31a328fbd3f632b526dfd706434f550
Jezreel Franklin <mavyjayann@gmail.com>
2024-10-22 15:02:17 +0100

    Add cache to planets service

--- src/handlers/planets.ts
+++ src/handlers/planets.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from "express";
 import { getPlanetById, getPlanets } from "../services/planetsApiService";
-import axios from "axios";
+
+import Cache from "../utils/cache";
+
+const cache = new Cache();
+
+const PLANETS_CACHE_KEY = "planets";
+const PLANET_CACHE_KEY = (id: string) => `planet-${id}`;
 
 export const getAllPlanets = async (req: Request, res: Response) => {
+  const planetsCache = cache.get(PLANETS_CACHE_KEY);
+
+  if (planetsCache) {
+    res.json(planetsCache);
+    return;
+  }
+
   const response = await getPlanets();
 
   if (!response.success) {
@@ -11,11 +24,21 @@ export const getAllPlanets = async (req: Request, res: Response) => {
     return;
   }
 
+  cache.set(PLANETS_CACHE_KEY, response);
+
   res.json(response);
 };
 
 export const getOnePlanet = async (req: Request, res: Response) => {
   const id = req.params.id;
+
+  const planetCache = cache.get(PLANET_CACHE_KEY(id));
+
+  if (planetCache) {
+    res.json(planetCache);
+    return;
+  }
+
   const response = await getPlanetById(id);
 
   if (!response.success) {
@@ -24,5 +47,6 @@ export const getOnePlanet = async (req: Request, res: Response) => {
     return;
   }
 
+  cache.set(PLANET_CACHE_KEY(id), response);
   res.json(response);
 };
